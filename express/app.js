04ebfdcef7944c4e60d4e5f75f69f524c85d935b
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-11 14:48:16 +0700

    route sederhana

--- express/app.js
+++ express/app.js
@@ -5,6 +5,17 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/about", (req, res) => {
+  res.send("Ini adalah halaman about");
+});
+app.get("/contact", (req, res) => {
+  res.send("Ini adalah halaman contact");
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("404 Not Found");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
